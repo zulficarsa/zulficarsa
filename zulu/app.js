485cfadea256a755d0106bfb98dce787434603dc
unknown <zulu1994zu@gmail.com>
2021-01-24 23:21:56 +0300

    fail of js and html

---
+++ zulu/app.js
@@ -0,0 +1,47 @@
+
+/*
+function zulu(){
+   "use strict"
+   var ss = document.getElementById("fass").value,
+       myf = 2*ss,
+      myu = document.getElementById("zu");
+            switch (ss) {
+              case ss=== "" :
+                 myu.innerHTML= "ليس  هنالك قيمة";
+              break;
+               case isNaN(ss):
+                   myu.innerHTML= "اكتب رقما ";
+                break;
+               default:myu.innerHTML= "قيمة الدالة هي " +myf;
+                break;
+}
+
+console.log(ss)
+
+
+};
+*/
+
+function ss(){
+ let myAge = function (x,y){
+   let z;
+   document.write("<select>");
+for(z=x;z<=y;z++){
+   document.write("<option value=\""+z+"\"> " + z +"</option>" );
+}
+
+   document.write("</select>");
+};
+
+
+let k = myAge(1900,2021);
+
+let j = myAge(1,12);
+let h = myAge(1,30);
+
+};
+ss()
+function age(){
+   return document.write('your age is : '+ myAge())
+ }
+ 
